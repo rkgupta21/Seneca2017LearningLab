2e0041165835356039d419ab953ce7ee296f33f3
Rahul Gupta <rahul.gupta94@hotmail.com>
2017-04-21 15:20:10 -0400

    Added test file

---
+++ seneca.test.js
@@ -0,0 +1,23 @@
+var seneca = require('./seneca');
+
+describe('seneca.isValidEmail()', function() {
+
+  test('returns true for simple myseneca address', function() {
+    var simpleEmail = 'someone@myseneca.ca';
+    expect(seneca.isValidEmail(simpleEmail)).toBe(true);
+  });
+
+  test('returns false for a non-myseneca address', function() {
+    var gmailAddress = 'someone@gmail.com';
+    expect(seneca.isValidEmail(gmailAddress)).toBe(false);
+  });
+});
+
+describe('seneca.formatSenecaEmail()', function() {
+
+  test('adds @myseneca.ca to the end of name', function() {
+    var name = "mshaw";
+    expect(seneca.formatSenecaEmail(name)).toBe('mshaw@myseneca.ca');
+  });
+});
+
