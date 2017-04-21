c355ae8e2686d63b762c35a9986469fcd417f449
Rahul Gupta <rahul.gupta94@hotmail.com>
2017-04-21 15:31:06 -0400

    Modified test cases

--- seneca.test.js
+++ seneca.test.js
@@ -16,8 +16,73 @@ describe('seneca.isValidEmail()', function() {
 describe('seneca.formatSenecaEmail()', function() {
 
   test('adds @myseneca.ca to the end of name', function() {
-    var name = "mshaw";
-    expect(seneca.formatSenecaEmail(name)).toBe('mshaw@myseneca.ca');
+    var name = "rkgupta2";
+    expect(seneca.formatSenecaEmail(name)).toBe('rkgupta2@myseneca.ca');
   });
 });
+
+test('returns false for a non-myseneca address', () => {
+    var gmailAddress = 'someone@gmail.com';
+    expect(seneca.isValidEmail(gmailAddress)).toBe(false);
+  });
+
+  test('returns false for a bool', () => {
+    var str = true;
+    expect(seneca.isValidEmail(str)).toBe(false);
+  });
+  test('return false for integer', () => {
+    var str = 2;
+    expect(seneca.isValidEmail(str)).toBe(false);
+  });
+ test('returns false for email containing leading whitespace', () => {
+
+    const str = ' rkgupta2@myseneca.ca';
+    expect(seneca.isValidEmail(str)).toBe(false);
+  });
+    test('returns true for old professor email', () => {
+
+    var str = 'david.humphrey@senecac.on.ca';
+    expect(seneca.isValidEmail(str)).toBe(true);
+  });
+
+  test('returns false for null string', () => {
+    var str = null;
+    expect(seneca.isValidEmail(str)).toBe(false);
+  });
+
+  test('returns true for professor email', () => {
+
+    var str = 'david.humphrey@senecacollege.ca';
+    expect(seneca.isValidEmail(str)).toBe(true);
+  });
+
+  test('returns false for empty string', () => {
+     var str = '';
+    expect(seneca.isValidEmail(str)).toBe(false);
+   });
+  
+  test('returns "Invalid" when name = empty ', () => {
+    var name = '';
+    expect(seneca.formatSenecaEmail(name)).toBe('Invalid');
+  });
+  test('adds @myseneca.ca to the end of name that contains spaces', () => {
+    var name = '   sgupta44';
+    expect(seneca.formatSenecaEmail(name)).toBe('sgupta44@myseneca.ca');
+  });
+  test('returns "Invalid" when name = null', () => {
+    var name = null;
+    expect(seneca.formatSenecaEmail(name)).toBe('Invalid');
+  });
+
+  test('returns "Invalid" when name = number', () => {
+    var name = 1;
+    expect(seneca.formatSenecaEmail(name)).toBe('Invalid');
+  });
+
+  test('returns "Invalid" when name = boolean', () => {
+    var name = true;
+    expect(seneca.formatSenecaEmail(name)).toBe('Invalid');
+  });
+
+
 
